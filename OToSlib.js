cd276d018054b4727e8507047a9cc106070cce7a
sanaehururu1200 <sanaehururu1200@gmail.com>
2017-06-14 17:57:45 +0900

    本体のヘッダーをアップロード

---
+++ OToSlib.js
@@ -0,0 +1,33 @@
+/*
+  ___   ______         _______
+ /   \ |      |  ___  /   ___/
+|     ||_    _| /   \ (   \_ 
+|  O  |  |  |  |  O  | \__  \
+|     |  |  |  |     | /  \  \
+!     !  |  |   \___/  \      |
+ \___/   l__j           \_____/
+  ____  ____  ____           
+ /    ||    \l    j          
+|  o  ||  o  )|  T           
+|     ||   _/ |  |           
+|  _  ||  |   |  |           
+|  |  ||  |   j  l           
+l__j__jl__j  |____j  
+  
+*/
+
+/*
+
+第一版 2016-01/25
+第二版 2017-06/14
+
+SH(えすえいち):
+GitHub:
+http://github.com/SH-OToS/
+
+sanaehururu1200:
+GitHub:http://github.com/sanaehururu1200/
+Twitter:http://twitter.com/OToS_SH/
+Google+:https://plus.google.com/u/0/+%E3%81%9F%E3%81%91%E3%81%AF%E3%82%8B0221/
+
+*/
